2051d6f4f90a8eb326d250e8e80e1f6e4a4d4ee9
devbyVeriTech <devbyveritech@gmail.com>
2023-06-12 18:52:27 +0100

    more

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,10 +1,6 @@
 <script>
-    
-    /** @type {import('../home/$types').LayoutData} */
-    import '../../app.css'
     import '../style.css'
     import Nav from '$lib/components/Nav/Nav.svelte';
-    import Footer from '$lib/components/Footer/Footer.svelte';
     import { fly } from 'svelte/transition';
 
 
@@ -15,30 +11,22 @@
     <Nav />
 </header>
 
-<div class="spacer"></div>
+<div class="space20"></div>
     <div class="conta"
-    in:fly={{x:-200, duration:300, delay:300}}
-    out:fly={{x:200, duration:300}}>
-        <div class="spacer"></div>
+    in:fly={{x:-200, duration:400}}
+    out:fly={{x:200, duration:400}}>
+        <div class="space20"></div>
         <slot/>
     </div>
 
 <footer>
     <div class="footer">
-        <div class="spade">
-            <Footer/>
-        </div>
+        <div class="spade"></div>
+        
+        
     </div>
-    <div class="space20"></div>
-    <div class="space20"></div>
 </footer>                                                                                                                                                                                                                                         
 
 <style>
 
-.spacer{
-    padding: 60px 0px;
-}
-.spade{
-    padding: 20px 0px;
-}
 </style>
