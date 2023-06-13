62ed885c80daa2bee231ed942e9c3750c0a187b1
devbyVeriTech <devbyveritech@gmail.com>
2023-06-13 20:54:56 +0100

    kk

--- src/routes/my-account/+layout.svelte
+++ src/routes/my-account/+layout.svelte
@@ -31,8 +31,5 @@
 </footer>                                                                                                                                                                                                                                         
 
 <style>
-    main{
-      
-    }
 
 </style>
