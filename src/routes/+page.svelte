39ab93efd17de11b038f9fc87a357555ff61e031
devbyVeriTech <devbyveritech@gmail.com>
2023-07-03 12:30:19 +0100

    pay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,58 +9,10 @@ let paymentAmount = "";
 let firstName = "";
 let lastName = "";
 
-/**@param {{ preventDefault: () => void; }} event*/
-
-let config = {
-
-	key: 'pk_test_803a6685b209a0bf43f351743cd9134bf1f36b9f',
-
-	// key: 'pk_live_b72fee4b04840e6fe2ad223432ed22a49e32fb90',
-
-	email: '',
-
-	amount: '',
-
-	currency: 'NGN',
-	
-	ref: ''+Math.floor((Math.random() * 1000000000) + 1),
-
-	// label: "Optional string that replaces customer email"
-
-	onClose: function(){
-
-	  alert('Window closed.');
-
-	},
-	
-	callback: function(/** @type {{reference :string; }} */ response){
-
-	  let message = 'Payment complete! Reference: ' + response.reference;
-
-	  alert(message);
-
-	}
-};
-
-	/**@param {{ preventDefault: () => void; }} event*/
-
-	
-		function payWithPaystack(event) {
-		  event.preventDefault();
-		  // @ts-ignore
-		  config.email = email;
-		  // @ts-ignore
-		  config.amount = parseFloat(paymentAmount)*100;
-	
-		  // @ts-ignore
-		  let handler = PaystackPop.setup(config);
-		  handler.openIframe();
-		};
 </script>
 
 
 <svelte:head>
-	<script src="https://js.paystack.co/v1/inline.js"></script>
   <title>
     Cherrydee Beatstore
 	</title>
